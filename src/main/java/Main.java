43898b8ac56e5b99af6640ea38695dbe8968c5ec
Vitaliy Murzin <murzinvit@yandex.ru>
2024-05-17 01:02:46 +0500

    init commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Ints calc = new IntsCalculator();
+        System.out.println(calc.mult(2, 3));
+        System.out.println(calc.pow(2, 3));
+        System.out.println(calc.sum(2, 3));
+    }
+}
